25ae8ce35332cddaefa492862fb43ca2b3b4f6bb
Alex Visca <alexvisca@gmail.com>
2022-12-04 10:48:14 -0800

    update dir

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,7 +2,7 @@
 # abort any errors (including if wait-for-it fails)
 set -e
 # change backend config file
-envsubst < /app/backend.conf.template > /app/src/backend.conf
+envsubst < /app/backend.conf.template > /app/backend.conf
 # wait for the database to be up
 if [ -n "$MYSQL_HOST" ]; then
     /usr/bin/wait-for-it "$MYSQL_HOST:${MYSQL_PORT:-3306}"
